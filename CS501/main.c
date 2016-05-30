af50e3a0657ff764537acc54c2602233d0aa7a96
James Goldstein <jamesdylangoldstein@gmail.com>
2016-05-30 11:38:09 -0700

    Initial Commit

---
+++ CS501/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS501
+//
+//  Created by James Dylan Goldstein on 5/30/16.
+//  Copyright © 2016 James Dylan Goldstein. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
