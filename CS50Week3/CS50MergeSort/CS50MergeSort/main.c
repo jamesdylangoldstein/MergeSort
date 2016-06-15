97bfbb44bcab1b243a054d6afc4483541e9b3b1e
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-14 19:33:31 -0700

    Adding loops

--- CS50Week3/CS50MergeSort/CS50MergeSort/main.c
+++ CS50Week3/CS50MergeSort/CS50MergeSort/main.c
@@ -11,41 +11,45 @@
 int main(int argc, const char * argv[]) {
     
     int arrayOfNumbers[] = {4, 3, 2, 1};
-    int bufferArray[] = {4, 3, 2, 1};
-    
-    for(int printCounter = 0; printCounter < 4; printCounter++)
+    int lengthOfArray = sizeof(arrayOfNumbers)/4;
+    int numberOfComparisons = lengthOfArray/2;
+    int bufferArray[lengthOfArray];
+
+    for(int printCounter = 0; printCounter < lengthOfArray; printCounter++)
     {
         printf("%i ", arrayOfNumbers[printCounter]);
     }
     printf("\n");
 
     // Digit 0 and 1 compared, switch places if necessary
-    if (arrayOfNumbers[0] > arrayOfNumbers[1])
+    int comparisonCounterArrayValue = 0;
+    
+    for (int comparisonCounter = 1; comparisonCounter <= numberOfComparisons; comparisonCounter++)
     {
-        int bufferInt = arrayOfNumbers[0];
-        arrayOfNumbers[0] = arrayOfNumbers[1];
-        arrayOfNumbers[1] = bufferInt;
+        if (arrayOfNumbers[comparisonCounterArrayValue] > arrayOfNumbers[comparisonCounterArrayValue + 1])
+        {
+            int bufferInt = arrayOfNumbers[comparisonCounterArrayValue];
+            arrayOfNumbers[comparisonCounterArrayValue] = arrayOfNumbers[comparisonCounterArrayValue + 1];
+            arrayOfNumbers[comparisonCounterArrayValue + 1] = bufferInt;
+        }
+        
+        comparisonCounterArrayValue += 2;
     }
     
-    if (arrayOfNumbers[2] > arrayOfNumbers[3])
-    {
-        int bufferInt = arrayOfNumbers[2];
-        arrayOfNumbers[2] = arrayOfNumbers[3];
-        arrayOfNumbers[3] = bufferInt;
-    }
-    
-    for(int printCounter = 0; printCounter < 4; printCounter++)
+    for(int printCounter = 0; printCounter < lengthOfArray; printCounter++)
     {
         printf("%i ", arrayOfNumbers[printCounter]);
     }
     printf("\n");
     
-    for(int bufferCounter = 0; bufferCounter < 4; bufferCounter++)
+    
+    
+    // Second round
+    
+    for(int bufferCounter = 0; bufferCounter < lengthOfArray; bufferCounter++)
     {
         bufferArray[bufferCounter] = arrayOfNumbers[bufferCounter];
     }
-    
-    // Second round
     
     if (bufferArray[0] > bufferArray[2])
     {
@@ -75,8 +79,6 @@ int main(int argc, const char * argv[]) {
     }
     else
     {
-        arrayOfNumbers[0] = bufferArray[0];
-        
         if(bufferArray[1] > bufferArray[2])
         {
             arrayOfNumbers[1] = bufferArray[2];
@@ -86,23 +88,12 @@ int main(int argc, const char * argv[]) {
                 arrayOfNumbers[2] = bufferArray[3];
                 arrayOfNumbers[3] = bufferArray[2];
             }
-            else
-            {
-                arrayOfNumbers[2] = bufferArray[2];
-                arrayOfNumbers[3] = bufferArray[3];
-            }
-        }
-        else
-        {
-            arrayOfNumbers[1] = bufferArray[1];
-            arrayOfNumbers[2] = bufferArray[2];
-            arrayOfNumbers[3] = bufferArray[3];
         }
     }
     
     
     
-    for(int printCounter = 0; printCounter < 4; printCounter++)
+    for(int printCounter = 0; printCounter < lengthOfArray; printCounter++)
     {
         printf("%i ", arrayOfNumbers[printCounter]);
     }
