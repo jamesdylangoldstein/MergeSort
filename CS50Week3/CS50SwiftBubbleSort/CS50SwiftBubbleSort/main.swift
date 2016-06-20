080b3b25c58d7c4fe860283320d0973af449fd42
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-20 10:32:26 -0700

    Bubble Sort In Swift

--- CS50Week3/CS50SwiftBubbleSort/CS50SwiftBubbleSort/main.swift
+++ CS50Week3/CS50SwiftBubbleSort/CS50SwiftBubbleSort/main.swift
@@ -8,57 +8,62 @@
 
 import Foundation
 
-var numberArray = [7, 6, 5, 4, 3, 2, 1]
+func bubbleSort (array: [Int]) {
+    var swapCounter: Int
+    var bubbleCounter: Int
+    var numToCompare: Int
+    var evenCounter: Int = 2
+    
+    repeat {
+        swapCounter = 0
+        
+        if (evenCounter % 2 == 0) {
+            bubbleCounter = 0
+            numToCompare = 1
+        }
+        else {
+            bubbleCounter = 1
+            numToCompare = 2
+        }
+        
+        while (bubbleCounter < numberArray.count) {
+            
+            if (numToCompare > bubbleCounter) {
+                if (numberArray[bubbleCounter] > numberArray[numToCompare]) {
+                    swapCounter += 1
+                    var intBuffer = numberArray[bubbleCounter]
+                    numberArray[bubbleCounter] = numberArray[numToCompare]
+                    numberArray[numToCompare] = intBuffer
+                }
+            }
+            else {
+                if (numberArray[bubbleCounter] < numberArray[numToCompare]) {
+                    swapCounter += 1
+                    var intBuffer = numberArray[numToCompare]
+                    numberArray[numToCompare] = numberArray[bubbleCounter]
+                    numberArray[bubbleCounter] = intBuffer
+                }
+            }
+            
+            bubbleCounter += 2
+            numToCompare += 2
+            
+            if (numToCompare > (numberArray.count - 1)) {
+                numToCompare = 0
+            }
+        }
+        
+        evenCounter += 1
+        
+    } while (swapCounter != 0)
+    
+}
 
-var swapCounter: Int
-var bubbleCounter: Int
-var numToCompare: Int
-var evenCounter: Int = 2
+var numberArray = [7, 6, 5, 4, 3, 2, 1]
 
 print (numberArray)
 
-repeat {
-    swapCounter = 0
-    
-    if (evenCounter % 2 == 0) {
-        bubbleCounter = 0
-        numToCompare = 1
-    }
-    else {
-        bubbleCounter = 1
-        numToCompare = 2
-    }
-    
-    while (bubbleCounter < numberArray.count) {
-    
-        if (numToCompare > bubbleCounter) {
-            if (numberArray[bubbleCounter] > numberArray[numToCompare]) {
-                swapCounter += 1
-                var intBuffer = numberArray[bubbleCounter]
-                numberArray[bubbleCounter] = numberArray[numToCompare]
-                numberArray[numToCompare] = intBuffer
-            }
-        }
-        else {
-            if (numberArray[bubbleCounter] < numberArray[numToCompare]) {
-                swapCounter += 1
-                var intBuffer = numberArray[numToCompare]
-                numberArray[numToCompare] = numberArray[bubbleCounter]
-                numberArray[bubbleCounter] = intBuffer
-            }
-        }
-        
-        bubbleCounter += 2
-        numToCompare += 2
-        
-        if (numToCompare > (numberArray.count - 1)) {
-            numToCompare = 0
-        }
-    }
-
-    evenCounter += 1
-    
-} while (swapCounter != 0)
+bubbleSort(numberArray)
 
 print (numberArray)
 
